000e61f5a5a4533dc2ee7cd892c1729edeb6eb76
OBones <obones@free.fr>
2026-08-02 22:41:59 +0200

    feat: output the currently encoded date/time, it helps debugging

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -95,6 +95,8 @@ namespace CelebWeather
 
             tm = *localtime(&t);
 
+            Serial.printf("Encoding current date/time: %d/%02d/%02d %02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min);
+
             quartets[0] = 0xF;
 
             if( tm.tm_hour < 10)
